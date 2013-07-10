6b7d2236af703f8167bbb14569f4cf19959a273d
Dominic Betts <dominicb@contentmaster.com>
2013-07-10 06:18:27 +0100

    Create sample app

---
+++ apps/WAAD_ACS_Sample/WAAD_ACS_Sample/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System.Security.Claims;
+using System.Web.Mvc;
+
+namespace WAAD_ACS_Sample.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      ClaimsPrincipal cp = ClaimsPrincipal.Current;
+      var givenNameClaim = cp.FindFirst(ClaimTypes.GivenName);
+      var surnameClaim = cp.FindFirst(ClaimTypes.Surname);
+
+      if (givenNameClaim == null | surnameClaim == null)
+      {
+        ViewBag.Message = string.Format("Oops, no useful claims available");
+      }
+      else
+      {
+        string fullname =
+             string.Format("{0} {1}", givenNameClaim.Value,
+             surnameClaim.Value);
+        ViewBag.Message = string.Format("Dear {0}, welcome to the WAAD ACS Sample App",
+             fullname);
+      }
+      
+
+      return View();
+    }
+
+    public ActionResult Manage()
+    {
+      ViewBag.Authorized = false;
+      if (!User.IsInRole("Managers"))
+      {
+        ViewBag.Message = "You are not authorized for this page!";
+      }
+      else
+      {
+        ViewBag.Authorized = true;
+        ViewBag.Message = "Your managers only page.";
+      }
+
+      return View();
+    }
+
+    public ActionResult About()
+    {
+      ViewBag.Message = "Your app description page.";
+
+      return View();
+    }
+
+    public ActionResult Contact()
+    {
+      ViewBag.Message = "Your contact page.";
+
+      return View();
+    }
+  }
+}
---
+++ apps/WAAD_ACS_Sample/WAAD_ACS_Sample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WAAD_ACS_Sample.MvcApplication" Language="C#" %>
