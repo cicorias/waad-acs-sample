76e7a6a4215e86a1da659e1c5b52da5da2528374
Dominic Betts <dominicb@contentmaster.com>
2013-07-11 06:19:18 +0100

    Script to change return url in ACS

---
+++ scripts/ChangeReturnURL.ps1
@@ -0,0 +1,25 @@
+﻿# Change the return URL for your relying party in ACS
+
+# Prerequisites:
+# Windows Azure PowerShell (http://go.microsoft.com/?linkid=9811175)
+
+# You must provide: 
+# - The new return URL for your relying party
+# - Your ACS namespace name
+# - Your ACS management key
+# - The ACS name for your relying party
+
+
+# To work with ACS
+Import-Module WAPPSCmdlets
+
+$AppAddress = '[the new return URL for your application]'
+$ACSNamespace = '[your ACS namespace name]'
+$ACSManagementkey = '[your ACS Management Key]'
+$RelyingPartyName = '[the name of your relying party in ACS]'
+
+#  Change the return URL for your relying party
+$relyingParty = Get-RelyingParty -ManagementKey $ACSManagementkey -Name $RelyingPartyName -Namespace $ACSNamespace
+$relyingParty.ReturnUrl = $AppAddress
+Remove-RelyingParty -Name $RelyingPartyName -ManagementKey $ACSManagementkey -Namespace $ACSNamespace
+Add-RelyingParty -RelyingParty $relyingParty -ManagementKey $ACSManagementkey -Namespace $ACSNamespace
